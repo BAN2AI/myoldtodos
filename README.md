726bd92010b8397183e392831c11f3e66dfe8095
ban2ai <puksakm@gmail.com>
2023-03-22 18:18:23 +0200

    a simple commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Todo list avec Laravel and Vue 2
+
+
+- [ ] Edit tasks / Creer des taches
+
+- [ ] Search tasks / Rechercher ou filtrer les taches
+
+
+
---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   adampuks <puksakm@gmail.com>
+ */
+
+
+require __DIR__.'/../bootstrap/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
